f37dd4fe949007e8c7de9daeaa4e7e58d92ac536
Yule Show <yuleshow@mac.com>
2020-04-04 08:54:17 -0700

    updated 20200403

--- batch/yuleshow-wine.sh
+++ batch/yuleshow-wine.sh
@@ -18,7 +18,8 @@ sudo dpkg -i multiarch-support_2.28-10_i386.deb
 rm multiarch-support_2.28-10_i386.deb
 sudo dpkg --add-architecture i386
 sudo apt --fix-broken install -y
-sudo apt install libnss-mdns:i386 python-gtk2 -y
+sudo apt install libnss-mdns:i386 -y
+sudo apt install python-gtk2 -y
 
 wget https://media.codeweavers.com/pub/crossover/cxlinux/demo/crossover_19.0.1-1.deb
 sudo dpkg -i crossover_*.deb
