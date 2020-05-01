7c20cb0fd0e601dea7956b1c43ee1bb2d0a20516
Yule Show <yuleshow@mac.com>
2020-04-30 21:43:28 -0700

    updated 20200430

--- virtualbox-guest.sh
+++ virtualbox-guest.sh
@@ -1,3 +1,3 @@
-sudo apt-get install virtualbox-guest-additions-iso
-mkdir ~/host
-sudo mount -t vboxsf -o uid=$UID,gid=$(id -g) yuleshow-share ~/host
+sudo apt-get install virtualbox-guest-additions-iso -y
+mkdir ~/Desktop/master-client
+sudo mount -t vboxsf -o uid=$UID,gid=$(id -g) yuleshow-share ~/Desktop/master-client
