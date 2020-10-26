dd3482ba18c1921b36fb09390cb887f2f62f17b5
Yule Show <yuleshow@mac.com>
2020-10-26 16:16:35 -0700

    20201026 updated

--- batch/yuleshow-dupeguru.sh
+++ batch/yuleshow-dupeguru.sh
@@ -1,10 +1,16 @@
-wget https://launchpad.net/~hsoft/+archive/ubuntu/ppa/+build/11261317/+files/dupeguru_4.0.3~xenial_amd64.deb
-sudo dpkg -i dupeguru_4.0.3~xenial_amd64.deb
-sudo apt --fix-broken install -y
-sudo cp /usr/share/dupeguru/core/pe/_cache.cpython-35m-x86_64-linux-gnu.so /usr/share/dupeguru/core/pe/_cache.cpython-36m-x86_64-linux-gnu.so
-sudo cp /usr/share/dupeguru/core/pe/_cache.cpython-35m-x86_64-linux-gnu.so /usr/share/dupeguru/core/pe/_cache.cpython-37m-x86_64-linux-gnu.so
-sudo cp /usr/share/dupeguru/core/pe/_block.cpython-35m-x86_64-linux-gnu.so   /usr/share/dupeguru/core/pe/_block.cpython-36m-x86_64-linux-gnu.so
-sudo cp /usr/share/dupeguru/core/pe/_block.cpython-35m-x86_64-linux-gnu.so   /usr/share/dupeguru/core/pe/_block.cpython-37m-x86_64-linux-gnu.so
-sudo cp /usr/share/dupeguru/qt/pe/_block_qt.cpython-35m-x86_64-linux-gnu.so /usr/share/dupeguru/qt/pe/_block_qt.cpython-36m-x86_64-linux-gnu.so
-sudo cp /usr/share/dupeguru/qt/pe/_block_qt.cpython-35m-x86_64-linux-gnu.so /usr/share/dupeguru/qt/pe/_block_qt.cpython-37m-x86_64-linux-gnu.so
-sudo rm dupeguru_4.0.3~xenial_amd64.deb
+# wget https://launchpad.net/~hsoft/+archive/ubuntu/ppa/+build/11261317/+files/dupeguru_4.0.3~xenial_amd64.deb
+# sudo dpkg -i dupeguru_4.0.3~xenial_amd64.deb
+# sudo apt --fix-broken install -y
+# sudo cp /usr/share/dupeguru/core/pe/_cache.cpython-35m-x86_64-linux-gnu.so /usr/share/dupeguru/core/pe/_cache.cpython-36m-x86_64-linux-gnu.so
+# sudo cp /usr/share/dupeguru/core/pe/_cache.cpython-35m-x86_64-linux-gnu.so /usr/share/dupeguru/core/pe/_cache.cpython-37m-x86_64-linux-gnu.so
+# sudo cp /usr/share/dupeguru/core/pe/_block.cpython-35m-x86_64-linux-gnu.so   /usr/share/dupeguru/core/pe/_block.cpython-36m-x86_64-linux-gnu.so
+# sudo cp /usr/share/dupeguru/core/pe/_block.cpython-35m-x86_64-linux-gnu.so   /usr/share/dupeguru/core/pe/_block.cpython-37m-x86_64-linux-gnu.so
+# sudo cp /usr/share/dupeguru/qt/pe/_block_qt.cpython-35m-x86_64-linux-gnu.so /usr/share/dupeguru/qt/pe/_block_qt.cpython-36m-x86_64-linux-gnu.so
+# sudo cp /usr/share/dupeguru/qt/pe/_block_qt.cpython-35m-x86_64-linux-gnu.so /usr/share/dupeguru/qt/pe/_block_qt.cpython-37m-x86_64-linux-gnu.so
+# sudo rm dupeguru_4.0.3~xenial_amd64.deb
+
+
+wget http://www.yuleshow.com/files/ubuntu/dupeguru-master_build_2020-04-16.tar.gz 
+tar -zxvf dupeguru-master_build_2020-04-16.tar.gz
+mv dupeguru-master_build_2020-04-16 ~/Applications
+sudo cp ~/ubuntu-configuration/desktops/dupeguru.desktop /usr/share/applications
