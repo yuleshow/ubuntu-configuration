389cb85ad3da1312d590563837692cac6ce47e57
Yule Show <yuleshow@mac.com>
2020-10-26 14:48:35 -0700

    20201026 updated

--- batch/yuleshow-wechat.sh
+++ batch/yuleshow-wechat.sh
@@ -9,12 +9,21 @@
 
 
 # Second way
-cd ~
-wget https://github.com/geeeeeeeeek/electronic-wechat/releases/download/V2.0/linux-x64.tar.gz
-tar -zxvf linux-x64.tar.gz
-mv electronic-wechat-linux-x64/ ~/Applications/
-sudo cp ~/ubuntu-configuration/desktops/wechat.desktop /usr/share/applications/
-rm linux-x64.tar.gz
+# cd ~
+# wget https://github.com/geeeeeeeeek/electronic-wechat/releases/download/V2.0/linux-x64.tar.gz
+# tar -zxvf linux-x64.tar.gz
+# mv electronic-wechat-linux-x64/ ~/Applications/
+# sudo cp ~/ubuntu-configuration/desktops/wechat.desktop /usr/share/applications/
+# rm linux-x64.tar.gz
 
 # Third way, can't input Chinese with iBus
 # sudo snap install electronic-wechat
+
+# Forth way
+cd ~
+wget http://www.yuleshow.com/files/ubuntu/wechat.tar.gz
+tar -zxvf wechat.tar.gz
+sudo mv opt/electronic-wechat-linux-x64 /opt
+sudo cp ~/ubuntu-configuration/desktops/wechat.desktop /usr/share/applications/
+rm wechat.tar.gz
+rmdir opt
