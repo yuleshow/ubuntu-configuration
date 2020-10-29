5dbac97e9b87fecc9e40d0a5bf04428521dd1c60
Yule Show <yuleshow@mac.com>
2020-10-28 21:40:52 -0700

    20201028 updated

--- batch/yuleshow-tor.sh
+++ batch/yuleshow-tor.sh
@@ -1,5 +1,6 @@
 cd ~/Applications
-wget https://www.torproject.org/dist/torbrowser/9.0.7/tor-browser-linux64-9.0.7_en-US.tar.xz
+# wget https://www.torproject.org/dist/torbrowser/9.0.7/tor-browser-linux64-9.0.7_en-US.tar.xz
+wget https://www.torproject.org/dist/torbrowser/10.0.2/tor-browser-linux64-10.0.2_en-US.tar.xz
 tar -xJf tor-browser-linux64-*.tar.xz
 sudo cp ~/ubuntu-configuration/desktops/Tor.desktop /usr/share/applications/
 rm tor-browser-linux64-*.tar.xz
--- batch/yuleshow-video.sh
+++ batch/yuleshow-video.sh
@@ -1,4 +1,8 @@
-sudo add-apt-repository ppa:stebbins/handbrake-releases
-sudo apt-get update
-sudo apt-get install handbrake-gtk -y
-sudo apt-get install handbrake-cli -y 
+wget https://launchpad.net/~stebbins/+archive/ubuntu/handbrake-releases/+files/handbrake-cli_1.3.3.1-zhb-1ppa1~focal1_amd64.deb
+wget https://launchpad.net/~stebbins/+archive/ubuntu/handbrake-releases/+files/handbrake-gtk_1.3.3.1-zhb-1ppa1~focal1_amd64.deb
+# sudo add-apt-repository ppa:stebbins/handbrake-releases
+# sudo apt-get update
+# sudo apt-get install handbrake-gtk -y
+# sudo apt-get install handbrake-cli -y 
+sudo dpkg -i *.deb
+sudo rm *.deb
