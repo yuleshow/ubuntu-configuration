a0ed488648cce3e64891a27e44601ec3f5c2be75
Yule Show <yuleshow@mac.com>
2020-10-28 18:18:45 -0700

    20201028 updated

--- batch/yuleshow-xdm.sh
+++ batch/yuleshow-xdm.sh
@@ -1,4 +1,5 @@
-wget https://ayera.dl.sourceforge.net/project/xdman/xdm-2018-x64.tar.xz
+# wget https://ayera.dl.sourceforge.net/project/xdman/xdm-2018-x64.tar.xz
+wget https://github.com/subhra74/xdm/releases/download/7.2.11/xdm-setup-7.2.11.tar.xz
 tar -xvf xdm*.tar.xz
 sudo ./install.sh
 rm install.sh
