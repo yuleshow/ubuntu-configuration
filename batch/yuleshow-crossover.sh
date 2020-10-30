e031d1ecda14359cd6d1fd8c2adbd35606b5a5cb
Yule Show <yuleshow@mac.com>
2020-10-30 11:43:48 -0700

    20201030 updated

--- batch/yuleshow-crossover.sh
+++ batch/yuleshow-crossover.sh
@@ -21,9 +21,14 @@ sudo apt --fix-broken install -y
 sudo apt install libnss-mdns:i386 -y
 sudo apt install python-gtk2 -y
 
-wget https://media.codeweavers.com/pub/crossover/cxlinux/demo/crossover_19.0.1-1.deb
+# https://media.codeweavers.com/pub/crossover/cxlinux/demo/crossover_20.0.0-1.deb
+# wget https://media.codeweavers.com/pub/crossover/cxlinux/demo/crossover_19.0.1-1.deb
+wget https://media.codeweavers.com/pub/crossover/cxlinux/demo/crossover_19.0.2-1.deb
 sudo dpkg -i crossover_*.deb
 rm crossover_*.deb
 sudo apt --fix-broken install -y
 sudo apt install -f
 sudo apt-get install libgsm1:i386 libopenal1:i386 libpulse0:i386 libudev1:i386 libxslt1.1:i386 -y
+sudo /opt/cxoffice/bin/cxfix missinglibsane missinglibsane.amd64
+
+sudo apt install libsane:i386
