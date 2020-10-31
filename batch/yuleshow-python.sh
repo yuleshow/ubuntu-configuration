7ba02186de83a0c28664123df4ab3cd15d474d71
Yule Show <yuleshow@mac.com>
2020-10-31 16:24:07 -0700

    20201031 updated

--- batch/yuleshow-python.sh
+++ batch/yuleshow-python.sh
@@ -1,6 +1,6 @@
-sudo apt install python
+sudo apt install python -y
 curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
-sudo apt python2 get-pip.py
+sudo python2 get-pip.py
 pip2 install chardet
 rm get-pip.py
 # sudo apt install python3-dev -y
