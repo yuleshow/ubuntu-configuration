6dce020d30c9c5188547d4e2dd5170509206d331
Yule Show <yuleshow@mac.com>
2021-05-15 16:17:49 -0700

    updated 20210515

--- batch/mint-installer.sh
+++ batch/mint-installer.sh
@@ -5,7 +5,8 @@ mkdir ~/mint-themes
 
         wget http://packages.linuxmint.com/pool/main/m/mint-y-icons/mint-y-icons_1.5.0_all.deb
 	wget http://packages.linuxmint.com/pool/main/m/mint-themes/mint-themes_1.8.7_all.deb
-        sudo dpkg -i *.deb
-        
+        sudo dpkg -i mint-x-icons_1.5.5_all.deb
+        sudo dpkg -i mint-y-icons_1.5.0_all.deb
+        sudo dpkg -i mint-themes_1.8.7_all.deb
 	cd ..
         sudo rm -rf mint-themes/
