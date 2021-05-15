7f19f4223c6f58b1c78c8ec4e6183c60b71e47af
Yule Show <yuleshow@mac.com>
2021-05-14 17:54:48 -0700

    updated 20210514

--- batch/yuleshow-crossover.sh
+++ batch/yuleshow-crossover.sh
@@ -13,13 +13,22 @@
 # wine ubuntu-configuration/windows-exe/mp3tagv289asetup.exe
 
 sudo apt install libc6 -y
+sudo apt install libc6-dev -y
+sudo apt install libc6:i386 -y
+sudo apt install libc6-dev:i386 -y
+sudo apt install libc6-i386 -y
+sudo apt install libc6-dev-i386 -y
+sudo apt install lib32z1 -y
+
+
 wget http://ftp.br.debian.org/debian/pool/main/g/glibc/multiarch-support_2.28-10_i386.deb
+sudo dpkg --add-architecture i386
 sudo dpkg -i multiarch-support_2.28-10_i386.deb
 rm multiarch-support_2.28-10_i386.deb
-sudo dpkg --add-architecture i386
-sudo apt --fix-broken install -y
+
+# sudo apt --fix-broken install -y
 sudo apt install libnss-mdns:i386 -y
-sudo apt install python-gtk2 -y
+# sudo apt install python-gtk2 -y
 
 # # https://media.codeweavers.com/pub/crossover/cxlinux/demo/crossover_20.0.0-1.deb
 # # wget https://media.codeweavers.com/pub/crossover/cxlinux/demo/crossover_19.0.1-1.deb
@@ -62,5 +71,5 @@ sudo apt install libxinerama1:i386 -y
 sudo apt install libxrandr2:i386  -y
 sudo apt install libxslt1.1:i386 -y
 
-sudo /opt/cxoffice/bin/cxfix missinglibsane missinglibsane.amd64
+sudo /opt/cxoffice/bin/cxfix missinglibsane missinglibsane.amd64 -y
 
