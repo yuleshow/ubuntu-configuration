bb0656c9d66a0bfdc0e7e8ee2b344b1a0c0c91f6
Yule Show <yuleshow@mac.com>
2021-06-15 15:47:09 -0700

    updated 20210615

--- batch/yuleshow-tools.sh
+++ batch/yuleshow-tools.sh
@@ -11,7 +11,8 @@ sudo apt install gimp-help-en -y
 
 sudo apt install djview -y
 sudo apt install audacity -y
-
+sudo apt install pulseaudio -y
+sudo apt install pavucontrol -y
 # for backup
 sudo apt install duplicity -y
 
@@ -24,7 +25,7 @@ sudo apt install alacarte -y
 sudo add-apt-repository ppa:flacon -y
 sudo apt install flacon -y
 
-sudo snap install --candidate freac
+# sudo snap install --candidate freac
 
 sudo apt install gparted -y
 sudo apt install synaptic -y
@@ -34,3 +35,16 @@ sudo apt install preload -y
 sudo apt install gufw -y
 
 sudo apt install locate -y
+
+if [ -d ~/Applications ]
+then
+    echo "~/Applications already exists"
+else
+    mkdir ~/Applications
+    echo "~/Applications doesn't exist, created one."
+fi
+cd ~/Applications
+
+wget https://github.com/enzo1982/freac/releases/download/v1.1.4/freac-1.1.4-linux-x86_64.AppImage
+chmod +x *.AppImage
+sudo cp ~/ubuntu-configuration/desktops/freac.desktop /usr/share/applications/
--- batch/yuleshow-virtualbox.sh
+++ batch/yuleshow-virtualbox.sh
@@ -6,7 +6,8 @@
 
 # wget https://download.virtualbox.org/virtualbox/6.1.12/virtualbox-6.1_6.1.12-139181~Ubuntu~eoan_amd64.deb
 # wget https://download.virtualbox.org/virtualbox/6.1.14/virtualbox-6.1_6.1.14-140239~Ubuntu~eoan_amd64.deb
-wget https://download.virtualbox.org/virtualbox/6.1.16/virtualbox-6.1_6.1.16-140961~Ubuntu~eoan_amd64.deb
+# wget https://download.virtualbox.org/virtualbox/6.1.16/virtualbox-6.1_6.1.16-140961~Ubuntu~eoan_amd64.deb
+wget https://download.virtualbox.org/virtualbox/6.1.22/virtualbox-6.1_6.1.22-144080~Ubuntu~eoan_amd64.deb
 sudo dpkg -i virtualbox*.deb
 rm virtualbox*.deb
 sudo apt --fix-broken install -y
