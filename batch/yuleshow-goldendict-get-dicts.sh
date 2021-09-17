b3c93b091c83da507fb98449e1a5c2b798982ba8
Yule Show <yuleshow@mac.com>
2021-09-17 10:20:28 -0700

    20210917 updated

---
+++ batch/yuleshow-goldendict-get-dicts.sh
@@ -0,0 +1,24 @@
+wget http://www.yuleshow.com/files/ubuntu/golden-dict/goldendict-dicts.tar.gz.partaa
+wget http://www.yuleshow.com/files/ubuntu/golden-dict/goldendict-dicts.tar.gz.partab
+wget http://www.yuleshow.com/files/ubuntu/golden-dict/goldendict-dicts.tar.gz.partac
+wget http://www.yuleshow.com/files/ubuntu/golden-dict/goldendict-dicts.tar.gz.partad
+wget http://www.yuleshow.com/files/ubuntu/golden-dict/goldendict-dicts.tar.gz.partae
+wget http://www.yuleshow.com/files/ubuntu/golden-dict/goldendict-dicts.tar.gz.partaf
+wget http://www.yuleshow.com/files/ubuntu/golden-dict/goldendict-dicts.tar.gz.partag
+wget http://www.yuleshow.com/files/ubuntu/golden-dict/goldendict-dicts.tar.gz.partah
+wget http://www.yuleshow.com/files/ubuntu/golden-dict/goldendict-dicts.tar.gz.partai
+wget http://www.yuleshow.com/files/ubuntu/golden-dict/goldendict-dicts.tar.gz.partaj
+wget http://www.yuleshow.com/files/ubuntu/golden-dict/goldendict-dicts.tar.gz.partak
+wget http://www.yuleshow.com/files/ubuntu/golden-dict/goldendict-dicts.tar.gz.partal
+wget http://www.yuleshow.com/files/ubuntu/golden-dict/goldendict-dicts.tar.gz.partam
+wget http://www.yuleshow.com/files/ubuntu/golden-dict/goldendict-dicts.tar.gz.partan
+wget http://www.yuleshow.com/files/ubuntu/golden-dict/goldendict-dicts.tar.gz.partao
+wget http://www.yuleshow.com/files/ubuntu/golden-dict/goldendict-dicts.tar.gz.partap
+wget http://www.yuleshow.com/files/ubuntu/golden-dict/goldendict-dicts.tar.gz.partaq
+wget http://www.yuleshow.com/files/ubuntu/golden-dict/goldendict-dicts.tar.gz.partar
+wget http://www.yuleshow.com/files/ubuntu/golden-dict/goldendict-dicts.tar.gz.partas
+wget http://www.yuleshow.com/files/ubuntu/golden-dict/goldendict-dicts.tar.gz.partat
+wget http://www.yuleshow.com/files/ubuntu/golden-dict/goldendict-dicts.tar.gz.partau
+wget http://www.yuleshow.com/files/ubuntu/golden-dict/goldendict-dicts.tar.gz.partav
+cat goldendict-dicts.tar.gz.part* > goldendict-dicts.tar.gz
+tar -zxf goldendict-dicts.tar.gz
