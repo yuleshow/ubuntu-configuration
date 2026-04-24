1f7d5bb4764e6f64f98bfe1e52cb90c6fcedbb58
yuleshow <3782164+yuleshow@users.noreply.github.com>
2026-04-23 18:20:56 -0700

    Update yuleshow-language.sh

--- batch/yuleshow-language.sh
+++ batch/yuleshow-language.sh
@@ -2,35 +2,16 @@
 # Simplified Chinese (zh-Hans / Unicode Chinese) are installed.
 
 # ---------------- English (US) ---------------------------------------------
-# en-US locale / hunspell / Firefox / Thunderbird
-sudo apt install language-pack-gnome-en-base -y
-sudo apt install language-pack-gnome-en -y
+sudo apt install hunspell -y
 sudo apt install hunspell-en-us -y
 sudo apt install hyphen-en-us -y
 sudo apt install mythes-en-us -y
-sudo apt install firefox-locale-en -y
-sudo apt install thunderbird-locale-en-us -y
-
-# ---------------- Traditional Chinese (zh-Hant) ----------------------------
-sudo apt install language-pack-gnome-zh-hant-base -y
-sudo apt install language-pack-gnome-zh-hant -y
-sudo apt install firefox-locale-zh-hant -y
-sudo apt install thunderbird-locale-zh-hant -y
-sudo apt install gnome-getting-started-docs-zh-hk -y
-sudo apt install fonts-arphic-uming -y
-sudo apt install fonts-arphic-ukai -y
-
-# ---------------- Simplified Chinese (zh-Hans) -----------------------------
-sudo apt install language-pack-gnome-zh-hans-base -y
-sudo apt install language-pack-gnome-zh-hans -y
-sudo apt install firefox-locale-zh-hans -y
-sudo apt install thunderbird-locale-zh-cn -y
 
 # ---------------- Shared CJK fonts -----------------------------------------
 sudo apt install fonts-noto-cjk -y
+
+
 sudo apt install fonts-noto-cjk-extra -y
 
 # ---------------- Input method (RIME) --------------------------------------
 sudo apt install ibus-rime -y
-# sudo apt install fcitx -y
-# sudo apt install fcitx-rime -y
